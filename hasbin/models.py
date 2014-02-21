380158f35d0a5d61da14ce9628f8cda204c3c00a
Bradford Powell <bpow@unc.edu>
2014-02-21 10:30:50 -0500

    models for DxLists

--- hasbin/models.py
+++ hasbin/models.py
@@ -47,3 +47,38 @@ make_hugo_list_field('omim_id', models.PositiveIntegerField(verbose_name="OMIM I
 make_hugo_list_field('prev_symbol', models.CharField(max_length=32, verbose_name='Previous Symbol'),)
 make_hugo_list_field('pubmed_id', models.PositiveIntegerField(verbose_name="Pubmed ID",),)
 make_hugo_list_field('refseq_accession', models.CharField(max_length=32,),)
+
+
+class DxList(models.Model):
+    name = models.CharField(max_length=64)
+    official = models.BooleanField()
+    last_update = models.DateTimeField(auto_now=True)
+    #last_update_by = models.ForeignKey(Users)
+
+
+class DxGene(models.Model):
+    TIER_CHOICES = list(enumerate((
+        'Do not include (0)',
+        'Diagnostic, high-priority (1)',
+        'Diagnostic, low-priority (2)',
+        'Research use only (3)',
+    )))
+    gene = models.CharField(max_length=32)  # TODO- maybe enforce as ForeignKey to HugoGene vs. allowing/showing mismatched data
+    phenotype = models.CharField(max_length=64)
+    tier = models.PositiveSmallIntegerField(choices=TIER_CHOICES, null=True)
+    syndromic = models.NullBooleanField()
+    last_update = models.DateTimeField(auto_now=True, editable=False,)
+    #last_update_by = models.ForeignKey(Users)
+    dxlist = models.ForeignKey(DxList, related_name='gene_phenotype_pairs')
+
+
+class DxGeneComment(models.Model):
+    dx_gene = models.ForeignKey(DxGene, related_name="comments")
+    comment = models.TextField()
+    when = models.DateTimeField(auto_now=True, editable=False,)
+    #user = models.ForeignKey(Users)
+
+
+class DxGeneReference(models.Model):
+    dx_gene = models.ForeignKey(DxGene, related_name="references")
+    reference = models.TextField()
