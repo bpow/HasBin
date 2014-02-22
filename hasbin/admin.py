2a3b8dd93d0899db0e711259e69d4b3e3be3ba6d
Bradford Powell <bpow@unc.edu>
2014-02-22 12:11:01 -0500

    custom admin classes with inline formsets for Binned genes and Dx genes

--- hasbin/admin.py
+++ hasbin/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 import hasbin.models as models
 
-admin.site.register((
-    models.HugoGene,
-    models.DxList,
-    models.BinnedGene,
-))
+
+class DxGeneReferenceInline(admin.TabularInline):
+    model = models.DxGeneReference
+
+
+class DxGeneCommentInline(admin.TabularInline):
+    model = models.DxGeneComment
+
+
+class DxGeneAdmin(admin.ModelAdmin):
+    inlines = [DxGeneReferenceInline, DxGeneCommentInline]
+
+
+class BinnedGeneReferenceInline(admin.TabularInline):
+    model = models.BinnedGeneReference
+
+
+class BinnedGeneCommentInline(admin.TabularInline):
+    model = models.BinnedGeneComment
+
+
+class BinnedGeneAdmin(admin.ModelAdmin):
+    inlines = [BinnedGeneReferenceInline, BinnedGeneCommentInline]
+
+
+admin.site.register(models.DxGene, DxGeneAdmin)
+admin.site.register(models.BinnedGene, BinnedGeneAdmin)
+
