ac68c50b3117d3b47ecd5384711265b53a80a51a
Bradford Powell <bpow@drpowell.org>
2014-06-05 09:39:49 -0400

    playing with rest

--- proj_hasbin/settings.py
+++ proj_hasbin/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'simple_history',
     'autocomplete_light',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,6 +84,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 try:
     from local_settings import *
--- proj_hasbin/urls.py
+++ proj_hasbin/urls.py
@@ -1,10 +1,23 @@
 from django.conf.urls import patterns, include, url
+from hasbin.models import HugoGene, DxList, DxGene
+from rest_framework import viewsets, routers
 
 import autocomplete_light
 autocomplete_light.autodiscover()
 
 from django.contrib import admin
 admin.autodiscover()
+
+class HugoGeneViewSet(viewsets.ModelViewSet):
+    model = HugoGene
+
+class DxGeneViewSet(viewsets.ModelViewSet):
+    model = DxGene
+
+router = routers.DefaultRouter()
+
+router.register(r'genes', HugoGeneViewSet)
+router.register(r'dxgenes', DxGeneViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +26,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^autocomplete/', include('autocomplete_light.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
     #url(r'^', include('hasbin.urls')),
 )
