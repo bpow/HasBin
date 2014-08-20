0fa1650a5d298c22b145684fbca9bb555ca1cec7
Bradford Powell <bpow@drpowell.org>
2014-08-20 15:34:31 -0400

    now with more Serializers!

--- hasbin/serializers.py
+++ hasbin/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+import autocomplete_light
 from .models import DxGene, DxGeneComment, DxList, HugoGene
 
 class DxListSerializer(serializers.HyperlinkedModelSerializer):
@@ -6,3 +7,14 @@ class DxListSerializer(serializers.HyperlinkedModelSerializer):
         model = DxList
         fields = 'name description version official gene_phenotype_pairs'.split()
         depth = 2
+
+class DxGeneSerializer(serializers.HyperlinkedModelSerializer):
+	hugo_gene = serializers.ChoiceField(
+		widget=autocomplete_light.TextWidget('HugoGeneAutocomplete'))
+	class Meta:
+		model = DxGene
+		depth = 2
+
+class HugoGeneSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = HugoGene
--- hasbin/views.py
+++ hasbin/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import HugoGene, DxGene, DxList
-from .serializers import DxListSerializer
+from .serializers import DxListSerializer, DxGeneSerializer, HugoGeneSerializer
 from rest_framework import viewsets
 
-class HugoGeneViewSet(viewsets.ModelViewSet):
+class HugoGeneViewSet(viewsets.ReadOnlyModelViewSet):
     model = HugoGene
+    serializer_class = HugoGeneSerializer
 
 class DxGeneViewSet(viewsets.ModelViewSet):
     model = DxGene
+    serializer_class = DxGeneSerializer
 
 class DxListViewSet(viewsets.ModelViewSet):
     model = DxList
